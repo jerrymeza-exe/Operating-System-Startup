922532d55ada710bdf71466f1418456accc69f28
Jerry Meza <148172647+jerrymeza-exe@users.noreply.github.com>
2025-04-13 21:35:18 -0500

    Update scheduler.cpp

--- scheduler.cpp
+++ scheduler.cpp
@@ -1,5 +1,5 @@
-
 #include "scheduler.h"
+#include "memory.h"
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -51,7 +51,7 @@ void Scheduling::moveAgingProcess(vector<Process>& fromQueue, vector<Process>& t
     }
 }
 
-void Scheduling::runSimulation(int totalProcesses, bool preemptiveSjf = false) {
+void Scheduling::runSimulation(int totalProcesses, bool preemptiveSjf) {
 
     srand(time(0));
 
@@ -61,6 +61,8 @@ void Scheduling::runSimulation(int totalProcesses, bool preemptiveSjf = false) {
     vector<Process> waitQueue;
     vector<Process> doneProcesses;
 
+    VirtualMemory memoryManager;
+
     int cycle = 0;
     int maxCycles = rand() % 50 + 30;
     int procCount = 1;
@@ -68,6 +70,7 @@ void Scheduling::runSimulation(int totalProcesses, bool preemptiveSjf = false) {
 
     while (cycle < maxCycles || highQueue.size() > 0 || midQueue.size() > 0 || lowQueue.size() > 0 || waitQueue.size() > 0) {
 
+        // I/O waiting queue handler
         size_t i = 0;
         while (i < waitQueue.size()) {
             waitQueue[i].decrementIoTime();
@@ -93,12 +96,26 @@ void Scheduling::runSimulation(int totalProcesses, bool preemptiveSjf = false) {
             }
         }
 
+        // Create new processes dynamically
         if (cycle % randomInterval == 0 && procCount <= totalProcesses) {
             int burstTime = rand() % 10 + 1;
             int priorityLevel = rand() % 3 + 1;
             int memoryNeeded = rand() % 1024 + 256;
             bool hasIO = rand() % 2;
 
+            //Allocate memory
+            if (!memoryManager.allocateMemory(procCount, memoryNeeded)) {
+                cout << "[Cycle " << cycle << "] Skipping Process " << procCount << " due to memory allocation failure.\n";
+                procCount++;
+                cycle++;
+                continue;
+            }
+
+            // Logical address translation after process creation
+            int sampleLogicalAddress = rand() % memoryNeeded;
+            memoryManager.translateAddress(procCount, sampleLogicalAddress);
+
+            // Create and queue process
             Process p(procCount, cycle, burstTime, priorityLevel, memoryNeeded, hasIO);
             p.updateState("Ready");
             p.setWaitingSinceCycle(cycle);
@@ -114,14 +131,17 @@ void Scheduling::runSimulation(int totalProcesses, bool preemptiveSjf = false) {
             cout << "\n[Cycle " << cycle << "] Process " << procCount
                  << " created (Burst Time: " << burstTime
                  << ", Priority: " << priorityLevel
-                 << ", I/O: " << (hasIO ? "Yes" : "No") << ")." << endl;
+                 << ", I/O: " << (hasIO ? "Yes" : "No")
+                 << ", Memory: " << memoryNeeded << " KB)." << endl;
 
             procCount++;
         }
 
+        // Aging
         moveAgingProcess(lowQueue, midQueue, cycle, 2);
         moveAgingProcess(midQueue, highQueue, cycle, 1);
 
+        // Sort queues
         sortFcfs(highQueue);
         sortFcfs(midQueue);
         if (preemptiveSjf) {
@@ -130,25 +150,25 @@ void Scheduling::runSimulation(int totalProcesses, bool preemptiveSjf = false) {
             sortSjf(lowQueue);
         }
 
-        Process* currentProc = NULL;
-        vector<Process>* currentQueue = NULL;
+        // Pick process from highest available queue
+        Process* currentProc = nullptr;
+        vector<Process>* currentQueue = nullptr;
 
-        if (highQueue.size() > 0) {
+        if (!highQueue.empty()) {
             currentProc = &highQueue.front();
             currentQueue = &highQueue;
-        } else if (midQueue.size() > 0) {
+        } else if (!midQueue.empty()) {
             currentProc = &midQueue.front();
             currentQueue = &midQueue;
-        } else if (lowQueue.size() > 0) {
+        } else if (!lowQueue.empty()) {
             currentProc = &lowQueue.front();
             currentQueue = &lowQueue;
         }
 
-        if (currentProc != NULL) {
+        if (currentProc != nullptr) {
             if (currentProc->getState() != "Running") {
                 currentProc->updateState("Running");
                 currentProc->setWaitingSinceCycle(-1);
-
                 cout << "[Cycle " << cycle << "] Process " << currentProc->getPid() << " is Running." << endl;
                 if (currentProc->hasIOOperations()) {
                     cout << "This process has I/O operations." << endl;
@@ -163,6 +183,7 @@ void Scheduling::runSimulation(int totalProcesses, bool preemptiveSjf = false) {
                 currentProc->updateState("Terminated");
                 cout << "[Cycle " << cycle + 1 << "] Process " << currentProc->getPid() << " Terminated." << endl;
                 currentProc->showProcess();
+                memoryManager.releaseMemory(currentProc->getPid()); // release memory on termination
                 doneProcesses.push_back(*currentProc);
                 currentQueue->erase(currentQueue->begin());
             } else if (currentProc->hasIOOperations()) {
@@ -177,6 +198,7 @@ void Scheduling::runSimulation(int totalProcesses, bool preemptiveSjf = false) {
             }
         }
 
+        memoryManager.showMemoryStatus();
         cycle++;
     }
 
@@ -190,10 +212,10 @@ void Scheduling::runSimulation(int totalProcesses, bool preemptiveSjf = false) {
         totalWaitingTime += doneProcesses[i].getWaitingTime();
     }
 
-    if (doneProcesses.size() > 0) {
+    if (!doneProcesses.empty()) {
         cout << "Average Turnaround Time: " << (float)totalTurnaroundTime / doneProcesses.size() << endl;
         cout << "Average Waiting Time: " << (float)totalWaitingTime / doneProcesses.size() << endl;
     } else {
-        cout << "No processes were finished. " << endl;
+        cout << "No processes were finished." << endl;
     }
 }
